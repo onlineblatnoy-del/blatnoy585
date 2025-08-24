051238e0204617776f222c2d305db9cf378d17d2
onlineblatnoy-del <onlineblatnoy@gmail.com>
2025-08-24 19:30:24 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-// oddiy tekshiruv
-echo "Openbudget vote bot is running!";
+// Agar URL da /hook.php bo‘lsa, hook.php ni ishga tushiramiz
+$request_uri = $_SERVER['REQUEST_URI'];
+
+if (strpos($request_uri, "hook.php") !== false) {
+    require __DIR__ . "/hook.php";
+    exit;
+}
+
+// Odatiy chiqish
+echo "✅ Openbudget vote bot is running!";
